05b6fd6ca16ec4d347d716785e09a277dcdf649b
gameworld <gameworld23@gmail.com>
2012-12-06 11:36:40 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <netdb.h>
 #include <time.h>
 #include <strings.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <sys/select.h>
 #include <sys/time.h>
@@ -14,17 +15,21 @@
 
 #define MAXCLIENTCOUNT   1024
 #define MAXBUFSIZE   10240
+#define MAXPATHNAME  1024
+#define MAXHEADLINE  20
 
 //a simple http server
 // store the client_fd and the recv buf;need send buf?
-// status 0: connected ,1:read head,2:head_read_complete,3:open_file_for_write? transfer,4:transfer
+// status 0: connected ,1:read head,2:head_read_complete,3:parse head ,4:open_file_for_write? transfer,5:transfer
 struct client{
     int clifd;
     int readfd;     //store the open file discription for client
     int status;     //status ? finish the request?
+    int httpstatus;
     int head_len;   // the current head data len
-    char head[MAXBUFSIZE];   //store the head of request
-    int recv_buf_len;
+    char header[MAXBUFSIZE];   //store the head of request
+    char * header_argv[HEADMAXLINE];   //parse the header,store the pointer in header
+    int  recv_buf_len;
     char recv_buf[MAXBUFSIZE];
     int  send_buf_len;
     char send_buf[MAXBUFSIZE];
@@ -34,8 +39,10 @@ struct client{
 // 读取http请求的头部
 int read_header(int clientfd);
 
-//解析http头部，主要是获取要请求的文件名,并打开它，返回打开的文件描述符
+//解析http头部，将头部分成argv数组
 int parse_head(int clientfd);
+//打开客户端请求的文件用于发送给客户端
+int open_filre(int clientfd);
 
 int make_server_listen_socket(int port,int backlog);
 
@@ -44,6 +51,9 @@ int process_request(int clientfd,FD_SET *fdset);
 int accept_conect(int listenfd,FD_SET *fdset);
 //read file ,wirte to the clientfd
 int response(int clientfd,FD_SET *fdset);
+
+//向客户端回复 的头部信息,写入http流,argc 要写入头部的参数个数，argc 参数的内容
+int sendheader(int clientfd,int argc,char **argv);
 
 //the client fd array,we now supported 1024 clients
 int cli_fd_array[1024];
@@ -165,14 +175,16 @@ int process_request(int clientfd,FD_SET *fdset)
     struct client* cliptr=client_array[clietnfd];
 
     int ret;
-    if(cliptr->status==1)
-       ret=read_header(clientfd);
-    if(ret==0)
-        response(clientfd,fdset);
-    //the connect has closed,clear the fd_set
-    if(ret==-2)
-        FD_CLR(clientfd,fdset)
-    
+    switch(cliptr->status)
+    {
+        case 1:ret=read_header(clientfd);
+               if(ret==0)
+                   response(clientfd,fdset);
+               if(ret==-2)
+                   FD_CLR(clientfd,fdset);
+
+    }
+
 
 
 
@@ -183,7 +195,13 @@ int response(int clientfd,FD_SET *fdset)
     struct client * cliptr=client_array[clientfd];
 
     //the head has_read complete
-    if(cliptr->status==2)
+    if(cliptr->status==4)
+        return -1;
+
+    if(cliptr->readfd<0){
+        printf("bad fd for read ");
+
+    }
 
 
 
@@ -214,38 +232,88 @@ int read_header(int clientfd)
 
     }
 
-
     //now we check the head is complete ?
     int start=cliptr->head_len-2;
     //make the start >=0
     if(start<0)
         start=0;
     int i;
-    int end=head_len+nread;
+    int end=cliptr->head_len+nread;
 
     for(i=start;i<head_len+nread;++i){
         if(i<=head_len+nread-4)
         if(cliptr->head[i]=='\r' && cliptr->head[i+1]=='\n' cliptr->head[i+2]=='\r' cliptr->head[i+3]=='\n'){
             //find the head;
+            cliptr->status=2;
+            cliptr->head_len=i+4;
             break;
         }
     }
-    if(i<=head_len+nread-4){
-        status=2;
-        return 0;
-    }
 
+    return 0;
 }
 
 int parse_head(int clientfd)
 {
+    struct client  * cliptr=client_array[clientfd];
+    if(cliptr->status!=2)
+        return -1;
 
+    char url[MAXBUFSIZE];
+    char filename[MAXPATHNAME];
+    head[MAXBUFSIZE-1]='\0';
+    char *tempstr=cliptr->header;
+    char *argv=cliptr->header_argv;
+    char *p;
+    int i=0;
 
+    while((p=strstr(tempstr,"\r\n"))!=NULL  && i<MAXHEADLINE){
+        *p='\0';
+        argv[i]=tempstr;
+        tempstr+=2;
+        i++;
+    }
+    argv[i]=NULL;
 
-
-
-
+    i=0;
+    //make a test
+    while(argv[i]!=NULL){
+        printf("%s",argv[i]);
+        ++i;
+    }
 }
+
+int open_file(int clientfd)
+{
+    struct client *cliptr=client_array[clientfd];
+
+    if(cliptr->status!=3)
+        return -1;
+    char url[MAXPATHNAME];
+    char pathname[MAXPATHNAME]; 
+    int fd=-1;
+    if(sscanf(argv[0],"GET %s HTTP/1.1",url)==1){
+        char *end=strstr(url,"?");
+        *end='\0';
+        snprintf(pathname,"./%s",url);
+        if((fd=open(pathname,O_RDONLY))<0){
+            fprintf(stderr,"open file %s error ",pathname);
+            perror("");
+            cliptr->httpstatus=404;
+            if((fd=open("404.html",O_RDONLY))<0){
+                fprintf(stderr,"open file  404 erro");
+                perror("");
+            }
+        }
+        cliptr->httpstatus=200;
+    }
+    cliptr->readfd=fd;
+    cliptr->status=4;
+    return 0;
+}
+
+
+
 
 
 
